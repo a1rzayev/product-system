190b85d4819a0802e1c16445e98b2852023b52e6
a1rzayev <asifrzayev2006@gmail.com>
2025-06-23 00:15:56 +0400

    Fix quantities in product page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -337,7 +337,7 @@ export default function ProductPage() {
                       id="quantity"
                       value={quantity}
                       onChange={(e) => setQuantity(parseInt(e.target.value))}
-                      className="rounded-md border border-gray-300 py-2 px-3 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                      className="rounded-md border border-gray-300 py-2 px-3 text-sm text-black focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                     >
                       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => (
                         <option key={num} value={num}>
